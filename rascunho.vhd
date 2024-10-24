ade6a79bd33c4b69a5e74d94fb5914ecc35af88a
Luís Fernando <luisfernandocvalentini@gmail.com>
2024-10-24 20:41:24 -0300

    codego

--- rascunho.vhd
+++ rascunho.vhd
@@ -14,33 +14,36 @@ architecture Behaviour of sistemaPortao is
 
     TYPE estado is (E0, E1, E2, E3);
     signal estado_atual: estado := E0;
-    signal A : std_logic := '0';
-    signal F : std_logic := '0';
 
 begin
 
     process(clk, B, S)
+
+    variable A : std_logic := '0';
+    variable F : std_logic := '0';
+
     begin
 
-        IF (S='1') THEN
-            IF (A='1' and F='0') THEN
-                A <= '0';
-                F <= '1';
-            ELSIF (A='0' and F='1') THEN
-                A <= '1';
-                F <= '0';
-            END IF;
-        ELSIF (B='1') THEN
-            IF (estado_atual=E0) THEN
-                F <= '0';
-                A <= '1';
-            ELSIF (estado_atual=E3) THEN
-                F <= '1';
-                A <= '0';
-            END IF;
-        END IF;
-
         IF (clk'event and clk='1') THEN
+
+            IF (S='1') THEN
+                IF (A='1' and F='0') THEN
+                    A := '0';
+                    F := '1';
+                ELSIF (A='0' and F='1') THEN
+                    A := '1';
+                    F := '0';
+                END IF;
+            ELSIF (B='1') THEN
+                IF (estado_atual=E0) THEN
+                    F := '0';
+                    A := '1';
+                ELSIF (estado_atual=E3) THEN
+                    F := '1';
+                    A := '0';
+                END IF;
+            END IF;
+
             IF (estado_atual=E0) THEN
                 IF (A='1' and F='0') THEN
                     estado_atual <= E1;
@@ -58,14 +61,14 @@ begin
                 IF (A='1' and F='0') THEN estado_atual <= E2;
                 ELSIF (A='0' and F='1') THEN
                     estado_atual <= E0;
-                    F <= '0';
+                    F := '0';
                     SF <= '1';
                 END IF;
             
             ELSE
                 IF (A='1' and F='0') THEN
                     estado_atual <= E3;
-                    A <= '0';
+                    A := '0';
                     SA <= '1';
                 ELSIF (A='0' and F='1') THEN estado_atual <= E1;
                 END IF;
